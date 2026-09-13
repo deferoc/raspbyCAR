5581b79c40012d6b73fd77a2d66d87c93837a46b
deferoc <defelicerocco@outlook.com>
2026-09-13 18:38:07 +0200

    v4

--- src/raspbycar/steering_motor.py
+++ src/raspbycar/steering_motor.py
@@ -30,11 +30,13 @@ class SteeringMotor:
         self.center()
 
     def set_angle(self, angle: float) -> None:
-        """Muove il servo all'angolo indicato e lo mantiene attivo."""
+        """Muove il servo all'angolo indicato; in centro rilascia per ridurre jitter."""
         if angle == self._last_angle:
             return
         self._servo.angle = angle
         sleep(config.STEERING_SETTLE_TIME)
+        if angle == config.STEERING_CENTER_ANGLE:
+            self._servo.detach()
         self._last_angle = angle
         self.current_angle = angle
 
